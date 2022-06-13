94f3c120c43c82d7def48a73d0ce4e3afc050e31
Harish Rane <51317454+AzadRane@users.noreply.github.com>
2022-06-13 16:05:08 +0530

    Update Sol.java

--- Sol.java
+++ Sol.java
@@ -91,3 +91,59 @@ public class Solution {
     
     }
 }
+======================================================================================================
+    OP ANSERR
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args)throws IOException {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    
+Scanner sc = new Scanner(System.in);
+    
+    int n = sc.nextInt();
+    
+    ArrayList<ArrayList<Integer>> aob = new ArrayList<ArrayList<Integer>>();
+    
+    for(int i=0;i<n;i++)
+    {
+        int size  = sc.nextInt();
+        
+        ArrayList<Integer> arr = new ArrayList<Integer>(size);
+        
+        for(int j=0;j<size;j++)
+        {
+            int value = sc.nextInt();
+            
+            arr.add(value);
+        }
+        
+        aob.add(arr);
+    }
+    
+    int m = sc.nextInt();
+    
+    for(int i=0;i<m;i++)
+    {
+        int r = sc.nextInt();
+        int col = sc.nextInt();
+        
+        try{
+            System.out.println(aob.get(r-1).get(col-1));
+        }
+        catch(Exception e)
+        {
+            System.out.println("ERROR!");
+        }    
+    }
+    
+    
+    
+    }
+}
